40894e77b820467ada8b2c9414124bffbb49c798
Harish77-github <125731197+Harish77-github@users.noreply.github.com>
2025-06-04 02:37:09 +0530

    Updated simon.js

--- simon.js
+++ simon.js
@@ -1,78 +1,28 @@
-let gameSeq = [];
-let userSeq = [];
-let btns = ["blue", "yellow", "plum", "aqua"];
-let started = false;
-let lvl = 0;
-let h3 = document.querySelector("h3");
-let body=document.querySelector("body");
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Simon Game</title>
+  <link rel="stylesheet" href="simon.css" />
+</head>
+<body>
+  <h2>Simon Game</h2>
+  <h3>Press Any key to Start the game</h3>
 
-document.addEventListener("keypress", function() {
-    if (!started) {
-        alert("Game is started!!");
-        started = true;
-        levelUp();
-    }
-});
+  <button id="themeToggle">Toggle Dark / Light Theme</button>
 
-function flashBtn(btn) {
-    btn.classList.add("flash");
-    setTimeout(function() {
-        btn.classList.remove("flash");
-    }, 500);
-}
+  <div class="btn-container">
+    <div class="line-one">
+      <div class="btn blue" id="blue">1</div>
+      <div class="btn yellow" id="yellow">2</div>
+    </div>
+    <div class="line-two">
+      <div class="btn plum" id="plum">3</div>
+      <div class="btn aqua" id="aqua">4</div>
+    </div>
+  </div>
 
-function userFlashBtn(btn) {
-    btn.classList.add("userflash");
-    setTimeout(function() {
-        btn.classList.remove("userflash");
-    }, 400);
-}
-
-function levelUp() {
-    userSeq = [];
-    lvl++;
-    h3.innerText = `Level ${lvl}`;
-    let randIdx = Math.floor(Math.random() * 4);
-    let randColor = btns[randIdx];
-    gameSeq.push(randColor);
-    let randBtn = document.querySelector(`.${randColor}`);
-    console.log(`Flashing button: ${randColor}`);
-    flashBtn(randBtn);
-}
-
-function checkAns(idx) {
-    if (userSeq[idx] === gameSeq[idx]) {
-        if (userSeq.length === gameSeq.length) {
-            setTimeout(levelUp, 1000);
-        }
-    } else {
-        h3.innerText = `Game over! , your previous score was ${lvl}.`;
-        h3.append(" press any key to start over again!");
-        body.style.backgroundColor="red";
-        setTimeout(function(){
-            body.style.backgroundColor="white";
-        },200)
-        reset();
-    }
-}
-function btnPress() {
-    let btn = this;
-    userFlashBtn(btn);
-    let userColor = btn.getAttribute("id");
-    userSeq.push(userColor);
-    checkAns(userSeq.length - 1);
-}
-
-let allBtns = document.querySelectorAll(".btn");
-for (let btn of allBtns) {
-    btn.addEventListener("click", btnPress);
-}
-function reset(){
-    started = false;
-    lvl = 0;
-    gameSeq = [];
-    userSeq=[];
-}
-function displayScore(){
-
-}
+  <script src="simon.js"></script>
+</body>
+</html>
